1d41112445b1e4ada084c281bd8251e42fb5e470
Dustin Sallings <dustin@spy.net>
2012-09-25 00:59:46 -0700

    A relatively useless treemap explorer prototype.

--- monitor/monitor.js
+++ monitor/monitor.js
@@ -182,3 +182,89 @@ function monitorInit() {
         d3.json(repCountURL, drawRepcounts);
     }, refreshInterval);
 }
+
+function showFileTreeMap(w, h, json1, json2) {
+    function cell() {
+        this.style("left", function(d) { return d.x + "px"; })
+            .style("top", function(d) { return d.y + "px"; })
+            .style("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
+            .style("height", function(d) { return Math.max(0, d.dy - 1) + "px"; })
+            .attr("title", function(d) {
+                return d.name + ": " + prettySize(d.size) +
+                    " / " + d.descendants + " descendants";
+            })
+            .on('click', function(x) {
+                history.pushState({path:x.path}, x.path, window.location + x.path);
+                updateFileMap(w, h, x.path);
+            });
+    }
+
+    function pathJoin(a, b) {
+        while (a.charAt(a.length-1) == "/") {
+            a = a.slice(0, -1);
+        }
+        while (b.charAt(0) == "/") {
+            b = b.slice(1);
+        }
+        return a + "/" + b;
+    }
+
+    var color = d3.scale.category20c();
+
+    var d3data = {name: json1.path, "children": []};
+
+    for (k in json2.dirs) {
+        var s = k.split('/');
+        s.pop();
+        var p = s.pop();
+
+        var ob = json2.dirs[k];
+        ob.path = pathJoin(json2.path, k);
+        var a = json1.dirs[p].children || [];
+        a.push(ob);
+        json1.dirs[p].children = a;
+    }
+
+    for (k in json1.dirs) {
+        d3data.children.push({
+            name: k,
+            children: json1.dirs[k],
+            size: json1.dirs[k].size,
+            descendants: json1.dirs[k].descendants,
+            path: pathJoin(json1.path, k)
+        });
+    }
+
+    var treemap = d3.layout.treemap()
+        .size([w, h])
+        .sticky(true)
+        .value(function(d) { return d.size; });
+
+    d3.select("#files").data([d3data]).selectAll("div")
+        .data(treemap.nodes)
+            .attr("class", "cell")
+        .style("background", function(d) { return color(d.name);})
+        .text(function(d) { return d.name; })
+        .call(cell)
+      .exit().remove();
+
+    d3.select("#files").data([d3data]).selectAll("div")
+        .data(treemap.nodes)
+      .enter().append("div")
+        .attr("class", "cell")
+        .style("background", function(d) { return color(d.name);})
+        .text(function(d) { return d.name; })
+        .call(cell);
+}
+
+function updateFileMap(w, h, path) {
+    d3.json("/.cbfs/list" + path, function(j1) {
+        d3.json("/.cbfs/list" + path + "?depth=2", function(j2) {
+            showFileTreeMap(w, h, j1, j2);
+        });
+    });
+}
+
+function fileInit(w, h, path) {
+    updateFileMap(w, h, path);
+}
