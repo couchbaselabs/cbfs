96d56d421783ac3af4da176330225e798a23aa20
Dustin Sallings <dustin@spy.net>
2012-09-30 18:48:47 -0700

    Show totals in ls -l

--- cbfsclient/ls.go
+++ cbfsclient/ls.go
@@ -129,6 +129,8 @@ func lsCommand(u string, args []string) {
 	filenames.Sort()
 
 	if *lsDashL {
+		totalFiles := 0
+		totalSize := uint64(0)
 		tw := tabwriter.NewWriter(os.Stdout, 2, 4, 2, ' ', 0)
 		for i := range dirnames {
 			dn := dirnames[i]
@@ -136,6 +138,8 @@ func lsCommand(u string, args []string) {
 			fmt.Fprintf(tw, "d %8s\t%s\t(%s descendants)\n",
 				humanize.Bytes(uint64(di.Size)), dn,
 				humanize.Comma(int64(di.Descendants)))
+			totalSize += uint64(di.Size)
+			totalFiles += di.Descendants
 		}
 		for i := range filenames {
 			fn := filenames[i]
@@ -143,7 +147,14 @@ func lsCommand(u string, args []string) {
 			fmt.Fprintf(tw, "f %8s\t%s\t%s\n",
 				humanize.Bytes(uint64(fi.Length)), fn,
 				fi.Headers.Get("Content-Type"))
+
+			totalSize += uint64(fi.Length)
+			totalFiles++
 		}
+		fmt.Fprintf(tw, "----------------------------------------\n")
+		fmt.Fprintf(tw, "Tot: %s\t\t%s files\n",
+			humanize.Bytes(totalSize),
+			humanize.Comma(int64(totalFiles)))
 		tw.Flush()
 	} else {
 		allnames := sort.StringSlice{}
