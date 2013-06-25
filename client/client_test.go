4e8046f37b2ee9b7107c77ed999d074b9c87da60
Dustin Sallings <dustin@spy.net>
2013-06-25 01:47:00 -0700

    Added test for path building.

---
+++ client/client_test.go
@@ -0,0 +1,27 @@
+package cbfsclient
+
+import (
+	"testing"
+)
+
+func TestPathGen(t *testing.T) {
+	c, err := New("http://cbfs:8484/")
+	if err != nil {
+		t.Fatalf("Error parsing thing: %v", err)
+	}
+
+	tests := map[string]string{
+		"":    "http://cbfs:8484/",
+		"a":   "http://cbfs:8484/a",
+		"/a":  "http://cbfs:8484/a",
+		"//a": "http://cbfs:8484/a",
+	}
+
+	for i, exp := range tests {
+		p := c.Path(i)
+		if p != exp {
+			t.Errorf("Expected %q for %q, got %q",
+				exp, i, p)
+		}
+	}
+}
