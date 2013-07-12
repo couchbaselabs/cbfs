4f6515ad13368bc4faaeab297e75f3cad2045eec
Dustin Sallings <dustin@spy.net>
2013-07-12 15:19:22 -0700

    Convert fileInfo to a template.

--- tools/cbfsclient/fileinfo.go
+++ tools/cbfsclient/fileinfo.go
@@ -2,19 +2,56 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strings"
+	"text/template"
 
 	"github.com/couchbaselabs/cbfs/client"
 	"github.com/couchbaselabs/cbfs/tools"
 )
 
 var fileInfoFlags = flag.NewFlagSet("fileInfo", flag.ExitOnError)
+var fileInfoTemplate = fileInfoFlags.String("t", "", "Display template")
+var fileInfoTemplateFile = fileInfoFlags.String("T", "", "Display template filename")
+
+const defaultFileInfoTemplate = `File: {{.Filename}}
+
+Headers:
+{{range $k, $v := .Header}}    {{$k}} = {{$v | join ","}}
+{{end}}
+
+Nodes:
+{{range $n, $t := .Nodes}}    {{$n}} ({{$t}})
+{{end}}`
 
 func fileInfoCommand(base string, args []string) {
 	fileInfoFlags.Parse(args)
+
+	tmplstr := *fileInfoTemplate
+	if tmplstr == "" {
+		switch *fileInfoTemplateFile {
+		case "":
+			tmplstr = defaultFileInfoTemplate
+		case "-":
+			td, err := ioutil.ReadAll(os.Stdin)
+			cbfstool.MaybeFatal(err, "Error reading template from stdin: %v", err)
+			tmplstr = string(td)
+		default:
+			td, err := ioutil.ReadFile(*fileInfoTemplateFile)
+			cbfstool.MaybeFatal(err, "Error reading template file: %v", err)
+			tmplstr = string(td)
+		}
+	}
+
+	tmpl, err := template.New("").Funcs(template.FuncMap{
+		"join": func(o string, s []string) string {
+			return strings.Join(s, o)
+		},
+	}).Parse(tmplstr)
+	cbfstool.MaybeFatal(err, "Error parsing template: %v", err)
 
 	u := cbfstool.ParseURL(base)
 	u.Path = args[0]
@@ -43,16 +80,10 @@ func fileInfoCommand(base string, args []string) {
 		log.Fatalf("Coudln't get blob info for %q: %v", h, err)
 	}
 
-	fmt.Printf("File: %q\n\n", u.Path[1:])
-
-	fmt.Printf("Headers:\n")
-
-	for k, vs := range res.Header {
-		fmt.Printf("\t%v = %v\n", k, strings.Join(vs, ", "))
-	}
-
-	fmt.Printf("\nNodes:\n")
-	for n, t := range infos[h].Nodes {
-		fmt.Printf("\t%v (%v)\n", n, t)
-	}
+	err = tmpl.Execute(os.Stdout, map[string]interface{}{
+		"Filename": u.Path[1:],
+		"Header":   res.Header,
+		"Nodes":    infos[h].Nodes,
+	})
+	cbfstool.MaybeFatal(err, "Error executing template: %v", err)
 }
