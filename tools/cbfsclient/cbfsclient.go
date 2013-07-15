8686e726893e2bf4da16df73476970af0b64ed50
Dustin Sallings <dustin@spy.net>
2013-07-15 15:53:46 -0700

    Allow single-arg download.
    I think arg parsing is broken in tool which led me to want to change
    this, but it's a good start.

--- tools/cbfsclient/cbfsclient.go
+++ tools/cbfsclient/cbfsclient.go
@@ -8,7 +8,7 @@ func main() {
 	cbfstool.ToolMain(
 		map[string]cbfstool.Command{
 			"upload":   {-2, uploadCommand, "/src/dir /dest/dir", uploadFlags},
-			"download": {2, downloadCommand, "/src/dir /dest/dir", dlFlags},
+			"download": {-1, downloadCommand, "/src/dir /dest/dir", dlFlags},
 			"ls":       {0, lsCommand, "[path]", lsFlags},
 			"rm":       {-1, rmCommand, "path", rmFlags},
 			"info":     {0, infoCommand, "", infoFlags},
